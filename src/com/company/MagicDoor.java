2dd4310a28b9c73e594b348b36c18a6059f1078f
Kojomkul <kojomkul@mail.ru>
2019-09-06 17:40:32 +0600

    new HW5

--- src/com/company/MagicDoor.java
+++ src/com/company/MagicDoor.java
@@ -1,5 +1,16 @@
 package com.company;
 
 public class MagicDoor {
-
+     public Hero [] herous (){
+         Hero [] heroes = new Hero[4];
+         Hero warrior = new Hero(250, 100, 30);
+         heroes [0] = warrior;
+         Hero psycho = new Hero(220,130,35);
+         heroes [1] = psycho;
+         Hero magic = new Hero(220,120,40);
+         heroes [2] = magic;
+         Hero medic = new Hero(200,50,25);
+         heroes [3] = medic;
+         return heroes;
+     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.company.Hero;
-
 public class Main {
 
     public static void main(String[] args) {
